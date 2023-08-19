367e1537fa075674bb6df325383d7870410988bd
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-08-19 21:02:34 +0900

    feat: spring project setting

---
+++ sns_service/src/main/java/joryu/sns_service/SnsServiceApplication.java
@@ -0,0 +1,13 @@
+package joryu.sns_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsServiceApplication.class, args);
+	}
+
+}
