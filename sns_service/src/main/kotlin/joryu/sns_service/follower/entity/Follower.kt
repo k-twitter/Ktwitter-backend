a129f79121c20e365c55830ae1ebe5b5aa289fcc
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-08-20 21:55:05 +0900

    feat: Follower Entity 생성

---
+++ sns_service/src/main/kotlin/joryu/sns_service/follower/entity/Follower.kt
@@ -0,0 +1,27 @@
+package joryu.sns_service.follower.entity
+
+import com.fasterxml.jackson.annotation.JsonBackReference
+import jakarta.persistence.*
+import joryu.sns_service.profile.entity.Profile
+
+@Table(name = "follower")
+@Entity
+data class Follower(
+        @Id
+        @Column(name = "follower_id")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long?,
+
+        @JsonBackReference
+        @ManyToOne
+        @JoinColumn(name = "profile_id")
+        val followingProfile: Profile?,
+
+        @Column(name = "follower_profile_id")
+        val followerProfileId: Long?
+) {
+    constructor() : this(null, null, null)
+    constructor(followingProfile: Profile, followerProfileId: Long) : this(null, followingProfile, followerProfileId) {
+        followingProfile.addFollower(this)
+    }
+}
