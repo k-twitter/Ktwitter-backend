2b31faa4b1a388121124419afebe464b53cbdefb
hyun98 <hyun0404woo@gmail.com>
2023-08-20 20:55:44 +0900

    fix error code
    1. add service transactional annotation
    2. add typo ChatMessage 'val'
    3. fix nullable field request message

--- sns_service/src/main/kotlin/joryu/sns_service/chat/service/ChatService.kt
+++ sns_service/src/main/kotlin/joryu/sns_service/chat/service/ChatService.kt
@@ -6,6 +6,7 @@ import joryu.sns_service.chat.repository.ChatRepository
 import joryu.sns_service.profile.repository.ProfileRepository
 import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +15,12 @@ class ChatService(
     private val profileRepository: ProfileRepository
 ){
 
+    @Transactional(readOnly = true)
     fun findAll(): List<ChatMessage?> {
         return chatRepository.findAll()
     }
 
+    @Transactional
     fun create(newMessage: NewMessageRequest) {
         val chatMessage = ChatMessage(newMessage.message, 1)
         chatRepository.save(chatMessage)
