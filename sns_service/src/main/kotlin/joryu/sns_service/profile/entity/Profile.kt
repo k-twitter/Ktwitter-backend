deefc7e8d1fbbc7fa5158c05ed40c91665d6e68b
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-08-20 21:55:19 +0900

    feat: follower - profile 연관관계 추가

--- sns_service/src/main/kotlin/joryu/sns_service/profile/entity/Profile.kt
+++ sns_service/src/main/kotlin/joryu/sns_service/profile/entity/Profile.kt
@@ -1,6 +1,8 @@
 package joryu.sns_service.profile.entity
 
+import com.fasterxml.jackson.annotation.JsonManagedReference
 import jakarta.persistence.*
+import joryu.sns_service.follower.entity.Follower
 import joryu.sns_service.profile.dto.request.ProfileUpdateRequest
 
 @Table(name = "profile")
@@ -8,8 +10,13 @@ import joryu.sns_service.profile.dto.request.ProfileUpdateRequest
 data class Profile(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "profile_id")
         val id: Long,
-        var name: String
+        var name: String,
+
+        @JsonManagedReference
+        @OneToMany(mappedBy = "followingProfile")
+        val followers: MutableList<Follower> = mutableListOf()
 ) {
     constructor() : this(0, "")
     constructor(name: String) : this(0, name)
@@ -17,4 +24,9 @@ data class Profile(
     fun update(profileUpdateRequest: ProfileUpdateRequest) {
         this.name = profileUpdateRequest.name
     }
+
+    fun addFollower(follower: Follower) {
+        followers.add(follower)
+    }
 }
+
