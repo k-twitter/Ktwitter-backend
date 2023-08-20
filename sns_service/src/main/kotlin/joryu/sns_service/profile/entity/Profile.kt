a1cc65a1508197a295b84a5bd39354b695b2d9bc
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-08-20 14:25:35 +0900

    update: Profile Entity 생성자 추가 및 nullable field 삭제

--- sns_service/src/main/kotlin/joryu/sns_service/profile/entity/Profile.kt
+++ sns_service/src/main/kotlin/joryu/sns_service/profile/entity/Profile.kt
@@ -1,20 +1,20 @@
 package joryu.sns_service.profile.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
-import lombok.Getter
+import jakarta.persistence.*
+import joryu.sns_service.profile.dto.request.ProfileUpdateRequest
 
 @Table(name = "profile")
-@Getter
 @Entity
 data class Profile(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null,
+        val id: Long,
         var name: String
 ) {
-        constructor(): this(null, "")
+    constructor() : this(0, "")
+    constructor(name: String) : this(0, name)
+
+    fun update(profileUpdateRequest: ProfileUpdateRequest) {
+        this.name = profileUpdateRequest.name
+    }
 }
