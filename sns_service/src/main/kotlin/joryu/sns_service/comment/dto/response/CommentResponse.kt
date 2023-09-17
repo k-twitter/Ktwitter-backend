c01551f402c81c7a9fb624abb395e0bab89521bd
정핸모 <gusah009@naver.com>
2023-09-17 22:57:13 +0900

    refactor: 댓글 응답필드 id -> commentId

--- sns_service/src/main/kotlin/joryu/sns_service/comment/dto/response/CommentResponse.kt
+++ sns_service/src/main/kotlin/joryu/sns_service/comment/dto/response/CommentResponse.kt
@@ -3,13 +3,13 @@ package joryu.sns_service.comment.dto.response
 import joryu.sns_service.comment.entity.Comment
 
 data class CommentResponse(
-    val id: Long,
+    val commentId: Long,
     val postId: Long,
     val parentCommentId: Long?,
     val content: String,
 ) {
     constructor(comment: Comment) : this(
-        id = comment.id,
+        commentId = comment.id,
         postId = comment.post.id,
         parentCommentId = comment.parent?.id,
         content = comment.content,
